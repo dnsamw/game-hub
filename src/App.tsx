22dbb01166fbb4dc59a3988e53a3cf9f7db51e84
dileepa-css <dileepa@ceylonsoft.jp>
2024-07-06 14:44:34 +0530

    fetch the games

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
@@ -18,10 +19,10 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main" bg="dodgerblue">
-        Main
+        <GameGrid />
       </GridItem> 
     </Grid>
   );
 }
 
-export default App;
+export default App; 
