04be451a68c7a3ad5cba776e8b38260e6b06102b
dileepa-css <dileepa@ceylonsoft.jp>
2024-07-07 08:42:55 +0530

    show spinner while fetching the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,15 @@
 import { useGenres } from "../hooks/useGenres";
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import { getCroppedImageUrl } from "../services/image-url";
 
 type Props = {};
 
 function GenreList({}: Props) {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if(isLoading) return <Spinner />;
+  if(error) return null;
+
   return (
     <List>
       {data.map((genre) => (
